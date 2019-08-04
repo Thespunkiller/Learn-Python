dd6277bc99916ee4eab233773e5b0863ee451a55
Thespunkiller <zaeed97@gmail.com>
2019-08-04 23:26:09 +0200

    math og strings

---
+++ From books/Python Programming for Beginners/Print_and_strings.py
@@ -0,0 +1,16 @@
+#===============================================
+#begge print quotes virker
+#print('hello')
+#print("hello")
+#===============================================
+#sætter ordet sammen
+#print("sør"+"en")
+#===============================================
+#laver det til en sætning med et mellemrum mellem vores int og string
+#print("i am", 21)
+#===============================================
+#escape chractors 
+#\
+#print('i\'m 21')
+#print("i\'m 21")
+#===============================================
