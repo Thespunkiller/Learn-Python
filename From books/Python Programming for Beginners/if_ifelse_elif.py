a356b98be723a2e21e132d20d471e2a7c8b28b43
Thespunkiller <zaeed97@gmail.com>
2019-08-05 03:31:39 +0200

    s

---
+++ From books/Python Programming for Beginners/if_ifelse_elif.py
@@ -0,0 +1,38 @@
+#===============================================
+#if sætninger 
+'''
+x = 2
+y = 7
+z = 10
+if x > y:
+    print(x,"is greater the", y)
+
+if x < y:
+    print(x,"is less the", y)
+
+if x == y:
+    print(x,"is the same", y)
+
+if x <= y:
+    print(x, "is the same or smaller then",y)
+    
+
+if z > y > x:
+    print(z ," is greater the", y , "is greater then", x)
+'''
+#===============================================
+#If else sætninger 
+'''
+x = 13 
+y = 6 
+
+if x < y:
+    print(x, "is less then",y)
+if x > y:
+    print(x,"is greater than",y)
+else:
+    print(x, "is not less then",y)
+'''
+#===============================================
+#elif   
+##missing
